98ca4d4bb4139320b2e05db293d2722dc37d2ff1
Christian <christian.reid007@gmail.com>
2023-06-18 15:58:13 +1200

    Begin development of sql tables and scripts

--- src/Components/cpa-sql.py
+++ src/Components/cpa-sql.py
@@ -8,3 +8,65 @@ driver='{ODBC Driver 18 for SQL Server}'
     
 conn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
 cur = conn.cursor()
+
+
+class Execute():
+    def payslip(self, username, date):
+        ID = f"WHERE [username] = {username}"
+        ID2 = f"WHERE [username] = {username} AND [period_end] = {date}"
+        return (
+            f"""
+            DECLARE @payRate AS DECIMAL(18,2) = (SELECT [pay_rate] FROM [pay_detail]s {ID} )
+            DECLARE @ordinaryPay AS BIT = (SELECT [alternative_hours] FROM [timesheet] {ID2})
+
+            INSERT INTO [payslip_data](
+                [username],
+                [pay_period_end],
+                [payment_1_type],
+                [pay_rate],
+                [payment_1_time_length],
+                [payment_1_name],
+                [payment_1_amount],
+                [pay_date],
+                [total_time_length],
+                [payment_2_name],
+                [payment_2_amount],
+                [total_pay],
+                [leave_taken],
+                [leave_start_date],
+                [leave_end_date],
+                [leave _days],
+                [leave_hours],
+                [leave_amount_paid],
+                [leave_type],
+                [total_leave_paid],
+                [leave_balance],
+                [pay_and_leave_amount],
+                [tax_allowance],
+                [nontax_allowance],
+                [one_off_pay],
+                [final_pay],
+                [gross_pay],
+                [year_to_date],
+                [deductions],
+                [paye],
+                [student_loan],
+                [child_support],
+                [tax_credit],
+                [net_pay],
+                [benefits]
+            )
+            VALUES(
+                {username},
+                (SELECT [period_end] FROM [timesheet] {ID2}),
+                (SELECT [pay_type] FROM [timesheet] {ID2}),
+                @payRate,
+                (SELECT [total_hours] FROM [timesheet] {ID2}),
+                ()
+
+                
+            );
+            """
+            )
+
+
