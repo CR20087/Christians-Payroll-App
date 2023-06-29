196fa87e70e46fcc502bfbbf6acb43b091fe8d8a
Christian <christian.reid007@gmail.com>
2023-06-30 08:18:02 +1200

    development of connecting to flsk app

---
+++ src/Components/login.jsx
@@ -0,0 +1,167 @@
+import styled from "styled-components";
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
+function LoginForm() {
+    const navigate = useNavigate()
+    const [isLoading, setIsLoading] = useState(false)
+    const [userName, setUserName] = useState()
+    const [password, setPassword] = useState()
+    const [isAuthorised, setIsAuthorised] = useState("")
+    const [data,setData] = useState([{}])
+    const [props,setProps] = useState([{}])
+  
+
+    const FetchLogin = (e) => {
+        e.preventDefault()
+  
+      setIsLoading(true)
+
+      setProps({
+        userName:userName,
+        password:password
+      })
+
+      const getLoginMatch = async (props) =>  {
+        const res = await fetch(`/login/'${props.userName}'/'${props.password}'`)
+        const data = await res.json()
+            setData(data)
+            console.log(data)
+            console.log(props.userName+' '+props.password)
+        }
+    
+        getLoginMatch(props);
+  
+      if (data.match === 'True') { 
+        const userID = userName 
+        setIsAuthorised('border-green')
+        
+        function randomString(length, chars) {
+          var result = '';
+          for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+          return result;
+        }
+  
+        const authKey = randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        sessionStorage.clear()
+        sessionStorage.setItem('authKey',authKey)
+        sessionStorage.setItem('userID', userID)
+  
+        navigate('/Portal/' + userID )      
+      } else if (data.match === 'False') {
+        setIsAuthorised('border-red')
+        console.log('Incorrect login details')
+      }
+  
+      setIsLoading(false)
+    }
+  
+    return (
+        <Window onSubmit={FetchLogin}>
+          <Head>
+            <img src="./Christian-Payroll-App-Logo.png" alt="logo" />
+              <p>Christian's Payroll App</p>
+          </Head>
+          <div className={isAuthorised} >
+            <p>User Name:</p>
+              <input 
+                onChange={(e) => setUserName(e.target.value)}
+                type="text"
+                spellCheck= "true"
+                placeholder="Enter Username" 
+                value={userName}
+              />
+            <p>Password:</p>
+              <input 
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                spellCheck= "true"
+                placeholder="Enter Password" 
+                value={password}
+              />
+          </div>
+          <button onClick={FetchLogin}>Login</button>
+          <div>{isLoading ? <Loading className="Show"/> : <Loading className="Hide"/> }</div>
+        </Window>      
+    )  
+  }
+  
+
+  
+  const Window = styled.button`
+    justify-self: center;
+    display: grid;
+    justify-content: center;
+    width: 35rem;
+    height: 40rem;
+    color: #313131;
+    background: #eedede;
+    border: 4px solid black;
+    font-weight: 600;
+    transition: 1s;
+    opacity: 0.92;
+    :hover {
+          opacity: 1;
+          width: 37rem;
+          height: 42rem;
+        }
+     input {
+      ::placeholder {
+        text-align: center;
+      }
+     }
+    .border-red {
+    input {
+      border: 1.5px solid red;
+    }}
+    .border-green {
+    input {
+      border: 1.5px solid green;
+    }}
+    button {
+      width: 5rem;
+      justify-self: center;
+      :hover {
+        background: #313131;
+        color: #eedede;
+      }
+    }
+  .Show {
+      visibility: visible;
+    }
+  .Hide {
+    visibility: hidden;
+  }`
+  
+  
+  const Loading = styled.div `
+    margin-left: 45%;
+    width: 40px;
+    height: 40px;
+    border: 3px solid #f3f3f3;
+    border-top: 3px solid #383636;
+    border-radius: 50%;
+    animation: spinner 1.5s linear infinite;
+    @keyframes spinner {
+      0% {
+      transform: rotate(0deg);
+      }
+      100% {
+      transform: rotate(360deg);
+      }
+    }
+  
+  `
+  
+  const Head = styled.div`
+    display: flex;
+  
+    p {
+    padding-top: 2rem;
+    }
+    img {
+      width: 35%;
+      height: 80%
+    }
+  `
+  export default LoginForm
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,63 +1,9 @@
 import styled from "styled-components";
-import {useState, useEffect} from 'react';
 import { BiArrowBack } from 'react-icons/bi'
-import { Await, Link, useNavigate} from "react-router-dom";
+import { Link } from "react-router-dom";
+import LoginForm from "../Components/login";
 
 function Login() {
-  const navigate = useNavigate()
-  const [isLoading, setIsLoading] = useState(false)
-  const [userName, setUserName] = useState()
-  const [password, setPassword] = useState()
-  const [isAuthorised, setIsAuthorised] = useState("")
-  const [data,setData] = useState([{}])
-  const [props,setProps] = useState([{}])
-  
-
-  useEffect(() => {
-    fetch(`/login/'${props.userName}'/'${props.password}'`).then(
-      res => res.json()
-      ).then(
-        data => {
-          setData(data)
-          console.log(data)
-          console.log(props.userName+' '+props.password)
-        }
-      )
-    }, [props]
-  )
-
-  function FetchLogin() {
-
-    setIsLoading(true)
-
-    setProps({
-      userName:userName,
-      password:password
-    })
-
-    if (data.match === 'True') { 
-      const userID = userName 
-      setIsAuthorised('border-green')
-      
-      function randomString(length, chars) {
-        var result = '';
-        for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-        return result;
-      }
-
-      const authKey = randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-      sessionStorage.clear()
-      sessionStorage.setItem('authKey',authKey)
-      sessionStorage.setItem('userID', userID)
-
-      navigate('/Portal/' + userID )      
-    } else if (data.match === 'False') {
-      setIsAuthorised('border-red')
-      console.log('Incorrect login details')
-    }
-
-    setIsLoading(false)
-  }
 
   return (
     <Page>
@@ -65,32 +11,7 @@ function Login() {
         <BiArrowBack size={50}></BiArrowBack>
         <p>Go Home</p>
       </GoBack>
-      <Window >
-        <Head>
-          <img src="./Christian-Payroll-App-Logo.png" alt="logo" />
-            <p>Christian's Payroll App</p>
-        </Head>
-        <div className={isAuthorised} >
-          <p>User Name:</p>
-            <input 
-              onChange={(e) => setUserName(e.target.value)}
-              type="text"
-              spellCheck= "true"
-              placeholder="Enter Username" 
-              value={userName}
-            />
-          <p>Password:</p>
-            <input 
-              onChange={(e) => setPassword(e.target.value)}
-              type="password"
-              spellCheck= "true"
-              placeholder="Enter Password" 
-              value={password}
-            />
-        </div>
-        <button onClick={FetchLogin}>Login</button>
-        <div>{isLoading ? <Loading className="Show"/> : <Loading className="Hide"/> }</div>
-      </Window>      
+      <LoginForm/>
     </Page> 
   )  
 }
@@ -105,51 +26,6 @@ p {
 }
 
 `
-
-const Window = styled.button`
-  justify-self: center;
-  display: grid;
-  justify-content: center;
-  width: 35rem;
-  height: 40rem;
-  color: #313131;
-  background: #eedede;
-  border: 4px solid black;
-  font-weight: 600;
-  transition: 1s;
-  opacity: 0.92;
-  :hover {
-        opacity: 1;
-        width: 37rem;
-        height: 42rem;
-      }
-   input {
-    ::placeholder {
-      text-align: center;
-    }
-   }
-  .border-red {
-  input {
-    border: 1.5px solid red;
-  }}
-  .border-green {
-  input {
-    border: 1.5px solid green;
-  }}
-  button {
-    width: 5rem;
-    justify-self: center;
-    :hover {
-      background: #313131;
-      color: #eedede;
-    }
-  }
-.Show {
-    visibility: visible;
-  }
-.Hide {
-  visibility: hidden;
-}`
 
 const GoBack = styled(Link)`
   display: flex;
@@ -167,34 +43,4 @@ const GoBack = styled(Link)`
   }
 `
 
-const Loading = styled.div `
-  margin-left: 45%;
-  width: 40px;
-  height: 40px;
-  border: 3px solid #f3f3f3;
-  border-top: 3px solid #383636;
-  border-radius: 50%;
-  animation: spinner 1.5s linear infinite;
-  @keyframes spinner {
-    0% {
-    transform: rotate(0deg);
-    }
-    100% {
-    transform: rotate(360deg);
-    }
-  }
-
-`
-
-const Head = styled.div`
-  display: flex;
-
-  p {
-  padding-top: 2rem;
-  }
-  img {
-    width: 35%;
-    height: 80%
-  }
-`
 export default Login
