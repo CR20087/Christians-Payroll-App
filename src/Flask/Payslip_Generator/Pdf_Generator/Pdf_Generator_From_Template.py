ef4fc4f19f62ce7132eaa2f78bd0253e5fb75ca3
Christian <christian.reid007@gmail.com>
2023-07-05 17:22:39 +1200

    testing /sendpayslip in flask on azure -13

--- src/Flask/Payslip_Generator/Pdf_Generator/Pdf_Generator_From_Template.py
+++ src/Flask/Payslip_Generator/Pdf_Generator/Pdf_Generator_From_Template.py
@@ -112,7 +112,7 @@ def pdf_generator_from_template():
     template_loader = jinja2.FileSystemLoader('/')
     template_env = jinja2.Environment(loader=template_loader)
 
-    template = template_env.get_template(os.path.join(os.getcwd(), 'PaySliptemplate.html'))
+    template = template_env.get_template(os.getcwd()+'src/Flask/Payslip_Generator/Pdf_Generator/PaySliptemplate.html')
     output_text = template.render(context)
 
     config = pdfkit.configuration(wkhtmltopdf='/src/Flask/Payslip_Generator/Pdf_Generator/wkhtmltopdf/bin/wkhtmltopdf.exe')  # Adjust the path to wkhtmltopdf
