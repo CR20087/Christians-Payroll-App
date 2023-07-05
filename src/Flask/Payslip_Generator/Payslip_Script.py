9121bc9720b546d39d179b4cf322ede6697afdbb
Christian <christian.reid007@gmail.com>
2023-07-05 13:44:12 +1200

    Fixed payslip script to work with flask -1

--- src/Flask/Payslip_Generator/Payslip_Script.py
+++ src/Flask/Payslip_Generator/Payslip_Script.py
@@ -1,15 +1,15 @@
 def Payslip_Script(username):
-    from Email_Sender import email_sender
-    from Pdf_Generator.Pdf_Generator_From_Template import pdf_generator_from_template
-    from ..cpa_sql import email_payslip_details
+    from . import Email_Sender  
+    from .Pdf_Generator import Pdf_Generator_From_Template
+    from .. import cpa_sql
 
     #Payslip Generate
 
-    pdf_generator_from_template()
+    Pdf_Generator_From_Template.pdf_generator_from_template()
 
     #get the data
 
-    results = email_payslip_details(username)
+    results = cpa_sql.email_payslip_details(username)
 
     # map the data
 
@@ -26,12 +26,7 @@ def Payslip_Script(username):
     recipient=recipient()
     manager=manager()
 
-    print(recipient.email)
-    print(recipient.name)
-    print(manager.name)
-    print(manager.contact)
-
-    email_sender(username,recipient,manager)
+    Email_Sender.email_sender(username,recipient,manager)
     return 'Success'
     
      
