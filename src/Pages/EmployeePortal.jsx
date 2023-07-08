f79cfe3dd8a01bcf003fb7aa69476f6f3a0f2578
Christian <christian.reid007@gmail.com>
2023-07-09 08:08:00 +1200

    Seperated portal into manager and employee portal

---
+++ src/Pages/EmployeePortal.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+import SideBar from "../Components/Sidebar"
+
+function EmployeePortal() {
+  return (
+    <Dashboard>
+    <SideBar/>
+    </Dashboard>
+  )
+}
+
+const Dashboard = styled.div``
+
+export default EmployeePortal
---
+++ src/Pages/ManagerPortal.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+import SideBar from "../Components/Sidebar"
+
+function ManagerPortal() {
+  return (
+    <Dashboard>
+    <SideBar/>
+    </Dashboard>
+  )
+}
+
+const Dashboard = styled.div``
+
+export default ManagerPortal
